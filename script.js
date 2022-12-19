a5beb8dc6bb1d144f141f2b5797fcb05208621b0
mint5150 <musia.home@gmail.com>
2022-12-19 21:26:57 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var string = "JavaScript";
+if (string === "JS") { // 条件A
+    console.log("条件Aの処理");
+    console.log(string);
+  } else if (string === "Java") { // 条件B
+    console.log("条件Bの処理");
+    console.log(string);
+  } else { // 条件Aと条件Bに当てはまらなかった時の処理
+    console.log("条件Aと条件B以外の処理");
+};
